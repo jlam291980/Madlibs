133c01f3431790aac31919c528630d0e5b1b188c
jlam291980 <97713661+jlam291980@users.noreply.github.com>
2022-01-20 16:45:58 -0800

    Add files via upload

---
+++ MadLibs.cpp
@@ -0,0 +1,65 @@
+// MadLibs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::cout << "Give a plural noun: " << "\n";
+    std::string pluralnoun1;
+    std::cin >> pluralnoun1;
+    std::cout << "Give an adjective: " << "\n";
+    std::string adjective1;
+    std::cin >> adjective1;
+    std::cout << "Give a noun: " << "\n";
+    std::string noun1;
+    std::cin >> noun1;
+    std::cout << "Give a noun: " << "\n";
+    std::string noun2;
+    std::cin >> noun2;
+    std::cout << "Give a noun: " << "\n";
+    std::string noun3;
+    std::cin >> noun3;
+    std::cout << "Give an adjective: " << "\n";
+    std::string adjective2;
+    std::cin >> adjective2;
+    std::cout << "Give a plural noun:" << "\n";
+    std::string pluralnoun2;
+    std::cin >> pluralnoun2;
+    std::cout << "Give an adjective: " << "\n";
+    std::string adjective3;
+    std::cin >> adjective3;
+    std::cout << "Give a noun: " << "\n";
+    std::string noun4;
+    std::cin >> noun4;
+    std::cout << "Give a noun: " << "\n";
+    std::string noun5;
+    std::cin >> noun5;
+    std::cout << "Give an adjective: " << "\n";
+    std::string adjective4;
+    std::cin >> adjective4;
+    std::cout << "Give a plural noun: " << "\n";
+    std::string pluralnoun3;
+    std::cin >> pluralnoun3;
+    std::cout << "Give a plural noun: " << "\n";
+    std::string pluralnoun4;
+    std::cin >> pluralnoun4;
+    std::cout << "Give a plural noun: " << "\n";
+    std::string pluralnoun5;
+    std::cin >> pluralnoun5;
+    std::cout << "Give a plural noun: " << "\n";
+    std::string pluralnoun6;
+    std::cin >> pluralnoun6;
+    std::cout << "\n";
+    std::cout << "So many " << pluralnoun1 << " " << "make " << adjective1 << " " << "pets!\n";
+    std::cout << "Many families prefer a furry " << noun1 << " " << "or a " << noun2 << " " << "to cuddle.\n";
+    std::cout << "Some people enjoy having a " << noun3 << " " << "for a pet, and still others prefer " << adjective2 + " " << pluralnoun2 + ".\n";
+    std::cout <<"Most pets need " << adjective3 << "care, like " << noun4 + ", " + noun5 + ", excercise, and a " << adjective4 + " " << "place to sleep.\n";
+    std::cout << "Some pets, like " << pluralnoun3 + " " << "live in " << pluralnoun4 + " " << "or " << pluralnoun5 + " " << "but almost everyone \n";
+    std::cout << "can agree that " << pluralnoun6 + " " <<"are fun for everyone!";
+    return 0;
+}
+
+
